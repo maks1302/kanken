99438ff2cba32b9586b177fd0393ca5ed8fc11b5
maks1302 <55560490+maks1302@users.noreply.github.com>
2021-03-19 16:41:44 +0200

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,214 @@
+﻿$(function() {
+	$('#carousel').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel2').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager2',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel3').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager3',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel4').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager4',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel5').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager5',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel6').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager6',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel7').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager7',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel8').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager8',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel9').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager9',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel10').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager10',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+$(function() {
+	$('#carousel11').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager11',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
+
+$(function() {
+	$('#carousel12').carouFredSel({
+		items: 1,
+		auto: false,
+		scroll: {
+			fx: 'none'
+		},
+		pagination: {
+			container: '#pager12',
+			event: 'mouseenter',
+			anchorBuilder: function(nr) {
+				var src = $(this).attr('src').replace('/item_img/', '/item_img/');
+				return '<img src="'+ src +'" border="0" />';
+			}
+		}				
+	});
+});
